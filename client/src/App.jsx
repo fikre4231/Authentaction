4d7c3576560460cf3ba981c0cc0f73769c340384
fikre4231 <fikrebereda@gmail.com>
2024-10-07 03:56:44 -0700

    mern stack auth

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl font-bold text-red-500 text-center hover:text-green-500 cursor-pointer'>Yitbarek</div>
+  )
+}
+
+export default App
