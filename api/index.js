fa71419c601aee7fff93452acb2b34174f8997bb
fikre4231 <fikrebereda@gmail.com>
2024-10-07 06:14:32 -0700

    controller and routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(() => console.log('DB connected')).catch(err => console.log(err))
@@ -8,3 +9,5 @@ mongoose.connect(process.env.MONGO).then(() => console.log('DB connected')).catc
 
 const app = express()
 app.listen(5000, () => console.log('Server started on port 5000!!'))
+
+app.use('/api/users', userRoutes )
---
+++ api/models/routes/user.route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+// import {
+//   test,
+//   updateUser,
+//   deleteUser,
+// } from '../controllers/user.controller.js';
+// import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.get('/', test);
+router.post('/update/:id', updateUser );
+router.delete('/delete/:id', deleteUser  );
+
+export default router;
